c2522c6112309db697ab0a3cec559a44d9164667
Paweł Łąk <pawellak@gmail.com>
2025-05-30 16:10:10 +0200

    add programmatically UIKit

---
+++ Bankey/Login/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  Bankey
+//
+//  Created by Paweł Łąk on 29/05/2025.
+//
+
+import Foundation
---
+++ Bankey/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Paweł Łąk on 28/05/2025.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
